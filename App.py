6a82f0b466646534bef7275dbfca34f1282c15a4
Juhoton <juho.k.nykanen@hotmail.com>
2022-12-21 02:16:40 +0200

    Added GUI for MainMenu and Game, but missing functionality. GameGUI has temp objects, and still missing Dices.

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 import random
+import time
 
 class MainMenuGUI():
-
     def __init__(self):
         self.window = tk.Tk()
 
@@ -25,24 +25,79 @@ class SavedGamesGUI():
     pass
 
 class GameGUI():
-
     def __init__(self):
+        self.player1State = PlayerGamestate()
+        self.player2State = PlayerGamestate()
+        self.players = [self.player1State, self.player2State]
+        self.Die1 = Die()
+        self.Die2 = Die()
+        self.Die3 = Die()
+        self.Die4 = Die()
+        self.Die5 = Die()
+        self.dices = [self.Die1, self.Die2, self.Die3, self.Die4, self.Die5]
+
         self.window = tk.Tk()
 
-        self.window.geometry("250x250")
+        self.window.geometry("500x800")
         self.window.title("Yatzy")
 
-        self.buttonFrame = tk.Frame(master=self.window, relief=tk.RIDGE, borderwidth=5)
+        self.scoreFrame = tk.Frame(master=self.window, background="black")
 
-        self.startGameButton = tk.Button(master=self.buttonFrame, text="Start new game", width=20, height=5)
-        self.startGameButton.pack()
+        self.leftSideLabels = ["YATZY","Ones","Twos","Threes","Fours","Fives","Sixes","Bonus","MidSum","One Pair","Two Pairs","Three of a Kind","Four of a Kind","Small straight","Large straight","Full house","Chance","Yatzy","Score"]
+        self.y = 0
+        self.x = 0
+        for sideLabel in self.leftSideLabels:
+            self.createFrame(self.x, self.y, "label", sideLabel)
+            self.y += 1
 
-        self.loadGameButton = tk.Button(master=self.buttonFrame, text="Load saved game", width=20, height=5)
-        self.loadGameButton.pack()
+        self.x += 1
+        for player in self.players:
+            self.y = 0
+            self.createFrame(self.x, self.y, "label", "Player")
+            self.y += 1
 
-        self.buttonFrame.place(relx=.5, rely=.5, anchor="center")
+            for score in player.upperSection:
+                self.createFrame(self.x, self.y, "button", score)
+                self.y += 1
+
+            self.createFrame(self.x, self.y, "label", player.bonus)
+            self.y += 1
+            self.createFrame(self.x, self.y, "label", player.midSum)
+            self.y += 1
+
+            for score in player.lowerSection:
+                self.createFrame(self.x, self.y, "button", score)
+                self.y += 1
+
+            self.createFrame(self.x, self.y, "label", player.score)
+            self.x += 1
+
+        self.scoreFrame.pack()
 
         self.window.mainloop()
+
+
+    def createFrame(self,x,y,type,text):
+        if type == "button":
+            frame = tk.Frame(master=self.scoreFrame,borderwidth=3)
+            frame.grid(row=y, column=x, padx=2, pady=2)
+            button = tk.Button(master=frame, text=text, width=15)
+            button.pack(fill=tk.X)
+
+        if type == "label":
+            frame = tk.Frame(master=self.scoreFrame,borderwidth=3)
+            frame.grid(row=y, column=x, padx=2, pady=2)
+            label = tk.Label(master=frame, text=text, width=15)
+            label.pack(fill=tk.X)
+
+
+        
+
+
+
+
+
+
 
 class PlayerGamestate:
     def __init__(self):
@@ -53,6 +108,9 @@ class PlayerGamestate:
         self.fives = None
         self.sixes = None
         self.upperSection = (self.ones, self.twos, self.threes, self.fours, self.fives, self.sixes)
+        self.bonus = None
+        self.midSum = None
+        
 
         self.onePair = None
         self.twoPairs = None
@@ -63,6 +121,7 @@ class PlayerGamestate:
         self.fullHouse = None
         self.chance = None
         self.yatzy = None
+        self.score= None
         self.lowerSection = (self.onePair, self.twoPairs, self.threeOfAKind, self.fourOfAKind, self.smallStraight, self.largeStraight, self.fullHouse, self.chance, self.yatzy)
 
 class Die:
@@ -70,12 +129,13 @@ class Die:
         self.rollDie()
 
 
-    def rollDie():
-        side = random.randint(1,6)
+    def rollDie(self):
+        self.side = random.randint(1,6)
+
+class Game:
+    pass
 
 
 
-
-
-
-
+#test1 = MainMenuGUI()
+test2 = GameGUI()
